7ef0c8557e58725da306a8e4409f34abf026d890
unknown <thecreatorof001@gmail.com>
2023-07-09 15:29:12 +0530

    my first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        socialBg:'#F5F7FB',
+        socialBlue: '#800080',
+      },
+    },
+  },
+  plugins: [],
+}
+
+//#F5F7FB
+//#218DFA
